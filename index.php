26531b8e074add672b590a44aedcefe7b3b8f66a
AjayBhutiya <ajaybhutiya2011@gmail.com>
2024-12-13 21:35:33 +0530

    login process

--- index.php
+++ index.php
@@ -1,25 +1,26 @@
 <?php
-include_once "helper/session.php";
+require_once "helper/Session.php";
 Session::init();
-define("root",'http://localhost/event/');
-include_once "helper/redirect.php";
-include_once "helper/DB.php";
+define('root', 'http://localhost/event/');
+require_once "helper/redirect.php";
+require_once "helper/DB.php";
 $module = "users";
-$file= "index";
+$file = "index";
 $uid=null;
-$url=$_GET['url'] ?? null;
-if($url){
-    $url = explode('/', rtrim($url,'/'));
+$url = $_GET['url'] ?? null;
+if ($url) {
+    $url = explode('/', rtrim($url, '/'));
     $module = $url[0];
     $file = $url[1] ?? $file;
     $uid = $url[2] ?? null;
 }
-$path="modules/$module/$file.php";
-if(file_exists($path)){
+$path = "modules/$module/$file.php";
+
+if (file_exists($path)) {
     include_once "header.php";
     include_once $path;
     include_once "footer.php";
-}else {
-redirect('404.php');
+
+}else{
+    redirect('404.php');
 }
-?>
